9107cbee811efeac54874faa0947da353ee44a93
URK21CS2004DELICIA <100010441+URK21CS2004DELICIA@users.noreply.github.com>
2022-08-16 23:07:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 class Solution:
     """This class implements linear queue.
-      Attributes:
-          stack: A list which maintains the content of stack.
-          queue: A list which maintains the content of queue.
-          top: An integer which denotes the index of the element at the top of the stack.
-          front: An integer which denotes the index of the element at the front of the queue.
-          rear: An integer which denotes the index of the element at the rear of the queue.
-          size: An integer which represents the size of stack and queue.
-      """
+    Attributes:
+    stack: A list which maintains the content of stack.
+    queue: A list which maintains the content of queue.
+    top: An integer which denotes the index of the element at the top of the stack.
+    front: An integer which denotes the index of the element at the front of the queue.
+    rear: An integer which denotes the index of the element at the rear of the queue.
+    size: An integer which represents the size of stack and queue.
+    """
 
     # Write your code here
     def _init_(self, size):
         """Inits Solution with stack, queue, size, top, front and rear.
         Arguments:
-          size: An integer to set the size of stack and queue.
+        size: An integer to set the size of stack and queue.
         """
         self.stack = []
         self.queue = []
